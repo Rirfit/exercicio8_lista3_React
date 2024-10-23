1c4c6ee0819001e31f7be2dd6790f4509645ae29
Rirfit <lilrirfit@gmail.com>
2024-10-23 17:54:42 -0300

    first commit

---
+++ exercicio8/src/App.jsx
@@ -0,0 +1,22 @@
+import Card from "./components/Card"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Card>
+        <h2>Titulo</h2>
+        <p>Conteúdo</p>
+      </Card>
+
+      <Card>
+        <h2>Titulo 2</h2>
+        <p>Conteúdo 2</p>
+      </Card>
+    </>
+  )
+}
+
+export default App
